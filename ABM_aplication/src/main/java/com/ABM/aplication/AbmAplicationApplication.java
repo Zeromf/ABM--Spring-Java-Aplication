432051e7746ca8dcdd6b798be2ab1beb3aae1401
Enzo Ferreyra <ferreyraenzo26@hotmail.com>
2020-10-28 04:32:15 -0300

    Comit inicial del proyecto. Close #1

---
+++ ABM_aplication/src/main/java/com/ABM/aplication/AbmAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.ABM.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmAplicationApplication.class, args);
+	}
+
+}
