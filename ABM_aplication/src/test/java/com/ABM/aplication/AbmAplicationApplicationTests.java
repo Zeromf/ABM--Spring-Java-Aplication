c00107ec5fee3a48d5416ae3d6459ce4fb975161
Enzo Ferreyra <ferreyraenzo26@hotmail.com>
2020-10-28 04:32:15 -0300

    Comit inicial del proyecto

---
+++ ABM_aplication/src/test/java/com/ABM/aplication/AbmAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ABM.aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
